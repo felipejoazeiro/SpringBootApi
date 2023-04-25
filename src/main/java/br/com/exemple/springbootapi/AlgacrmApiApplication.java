b8de2faf248593f0028759e477348d0fa1f3fa0f
Felipe Joazeiro <felipe.joazeiro@ictcondor.com.br>
2023-04-25 14:44:35 -0300

    Correção do versionamento do projeto. Adicionar e receber clientes funcionando.

--- src/main/java/br/com/exemple/springbootapi/AlgacrmApiApplication.java
+++ src/main/java/br/com/exemple/springbootapi/AlgacrmApiApplication.java
@@ -1,13 +1,12 @@
-package br.com.ictcondor.springbootapi;
+package br.com.exemple.springbootapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootApiApplication {
+public class AlgacrmApiApplication  {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootApiApplication.class, args);
+		SpringApplication.run(AlgacrmApiApplication.class, args);
 	}
-
 }
--- src/test/java/br/com/exemple/springbootapi/SpringBootApiApplicationTests.java
+++ src/test/java/br/com/exemple/springbootapi/SpringBootApiApplicationTests.java
@@ -1,4 +1,4 @@
-package br.com.ictcondor.springbootapi;
+package br.com.exemple.springbootapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
